c1df00970bff74f380373f3442462779866edb32
ameliablair <ameliablair@gmail.com>
2014-03-10 18:38:14 -0400

    added todo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todo::Application.config.secret_key_base = '6cabef9c051834a53d8d44d88d78aef090f629c58cf735a031e246da019e2c6af1bca1a45abf560af6fd909c8b6b5d703fa5e144d0d221729fccac74a49655da'
